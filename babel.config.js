6652529b94552bed71cb92450cf19b1cd74256c6
sdhosseinabadi <sdhosseinabadi@gmail.com>
2023-11-14 14:14:47 +0330

    fix

---
+++ babel.config.js
@@ -0,0 +1,22 @@
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "babel-plugin-inline-import",
+        {
+          extensions: [".cdc"],
+        },
+      ],
+      ["module:react-native-dotenv", {
+        "moduleName": "@env",
+        "path": ".env",
+        "blacklist": null,
+        "whitelist": null,
+        "safe": false,
+        "allowUndefined": true
+      }],
+    ],
+  };
+};
